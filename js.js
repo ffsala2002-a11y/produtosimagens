d8b7a3fd8b10994c29f5759c3b7e6b83523452eb
ffsala2002-a11y <ffsala2002@gmail.com>
2026-02-14 17:57:32 -0300

    Update js.js

--- js.js
+++ js.js
@@ -383,7 +383,7 @@ function renderLogin() {
 function renderHome() {
     return `
     <div class="container">
-    <h2>Imagens de Produtos</h2>
+    <h2>Imagens de Produtos AUG</h2>
     <div id="totalCatalogo"></div>
     <input class="input" placeholder="Buscar" id="filtro">
     <div class="grid" id="catalogGrid"></div>
